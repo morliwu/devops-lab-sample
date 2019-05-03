0059cd17f39124d90aab801255df8cf258a92cae
morliwu <50177763+morliwu@users.noreply.github.com>
2019-05-03 15:51:37 +0800

    Update HelloWorld.java

--- sample/pipeline-as-code/src/main/java/com/systex/HelloWorld.java
+++ sample/pipeline-as-code/src/main/java/com/systex/HelloWorld.java
@@ -2,6 +2,6 @@ package com.systex;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("TTTTTTTTTTTTTTTHello World! Hello World!");
+        System.out.println("AATTTTTTHello World! Hello World!");
     }
 }
